bf89cf05fe0b8f6a680ad254d69d114d9581dcea
Bartosz Kowynia <164752835+Bartosz-Kow@users.noreply.github.com>
2025-03-11 20:52:40 +0100

    Add welcome blank Template for WelcomeScreen

---
+++ components/templates/WelcomeTemplate.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+// STANIO TUTAJ ZBUDUJESZ NAM WELCOME SCREEN :)
+const WelcomeTemplate = () => {
+  return <View>Tu Bedzie nasz welcome screen</View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default WelcomeTemplate;
